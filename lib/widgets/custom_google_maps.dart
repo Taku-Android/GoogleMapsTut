798fda86c52cfeb69c0744717d6baa6310d4733c
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-02-17 21:42:05 +0200

    make a changeMapStyle Func to change style of map after read the json from files then using googleMaps controller

--- lib/widgets/custom_google_maps.dart
+++ lib/widgets/custom_google_maps.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_tut/widgets/custom_drawer.dart';
 
 class CustomGoogleMaps extends StatefulWidget {
   const CustomGoogleMaps({super.key});
@@ -11,7 +12,7 @@ class CustomGoogleMaps extends StatefulWidget {
 class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   late CameraPosition initialCameraPosition;
   late CameraTargetBounds cameraTargetBounds;
-  late GoogleMapController googleMapController ;
+  late GoogleMapController googleMapController;
 
   @override
   void initState() {
@@ -22,11 +23,27 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      drawer: CustomDrawer(
+        onNormalTap: () {
+          changeMapStyleToRetro(style: 'assets/map_styles/normal.json');
+        },
+        onRetroTap: () {
+          changeMapStyleToRetro(style: 'assets/map_styles/retro_style.json');
+        },
+        onNightTap: () {
+          changeMapStyleToRetro(style: 'assets/map_styles/night_style.json');
+        },
+      ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Stack(children: [
         GoogleMap(
-            onMapCreated: (controller){
-              googleMapController = controller ;
+            onMapCreated: (controller) {
+              googleMapController = controller;
             },
             cameraTargetBounds: cameraTargetBounds,
             initialCameraPosition: initialCameraPosition),
@@ -36,37 +53,42 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
           left: 16,
           child: FloatingActionButton(
               child: const Text('Change Location'),
-              onPressed: (){
-                googleMapController.animateCamera(CameraUpdate.newLatLng(LatLng(30.809448713427088, 29.118278000285414)));
-                setState(() {
-
-                });
+              onPressed: () {
+                onCameraBtnChangePress();
               }),
         )
-      ]
+      ]),
     );
   }
 
-
   initCameraPosition() {
     initialCameraPosition = const CameraPosition(
-      // world view 0 -> 3
-      // country view 4 -> 6
-      // city view 10 -> 13
-      // street view 14 -> 17  --- recommended
-      // building view 18 -> 20
+        // world view 0 -> 3
+        // country view 4 -> 6
+        // city view 10 -> 13
+        // street view 14 -> 17  --- recommended
+        // building view 18 -> 20
         zoom: 10.0,
         target: LatLng(31.2068347767131, 29.921081125421022));
   }
 
   initCameraBounds() {
-    cameraTargetBounds = CameraTargetBounds(
-        LatLngBounds(
-          southwest: const LatLng(30.831856335980262, 29.288566119991444),
-          northeast: const LatLng(31.33062249129825, 30.10630654553649),
-        ));
+    cameraTargetBounds = CameraTargetBounds(LatLngBounds(
+      southwest: const LatLng(30.831856335980262, 29.288566119991444),
+      northeast: const LatLng(31.33062249129825, 30.10630654553649),
+    ));
+  }
+
+  onCameraBtnChangePress() {
+    googleMapController.animateCamera(CameraUpdate.newLatLng(
+        const LatLng(30.809448713427088, 29.118278000285414)));
+    setState(() {});
+  }
+
+  changeMapStyleToRetro({required String style}) async {
+    var retroStyle = await DefaultAssetBundle.of(context).loadString(style);
+    googleMapController.setMapStyle(retroStyle);
   }
 }
-
 
 // 30.809448713427088, 29.118278000285414
