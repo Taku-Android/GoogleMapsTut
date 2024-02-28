c5b8a743b585f49b1627f236274f26998d194ab3
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-02-28 08:48:06 +0200

    adding another point to place marker

--- lib/model/place_model.dart
+++ lib/model/place_model.dart
@@ -18,4 +18,5 @@ List<PlaceModel> places = [
   PlaceModel(id: 2, name: 'Sediy Gaber Station', latLng: const LatLng(31.220000217871803, 29.941729018669385)),
   PlaceModel(id: 3, name: 'Four Seasons Hotel', latLng: const LatLng(31.246114612685993, 29.966297139036257)),
   PlaceModel(id: 4, name: 'Balbaa Restaurant', latLng: const LatLng(31.25611619866536, 29.990003220491054)),
+  PlaceModel(id: 5, name: 'عزبة سكينة', latLng: const LatLng(31.21255089602546, 29.992310097388717)),
 ];
