b10a7a43d5eeaf9163e538e675c028cf4cf75f79
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-02-29 07:14:44 +0200

    adding a new custom_live_location_tracking.dart to track location
    1 - first we need to check the location service is enabled or not
    
    2 - second to check if the user give us the location permission or not
    
    3 - now u can get the location using the stream as the location will be listen to get all the location if it changed

---
+++ lib/widgets/custom_live_location_tracking.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+class CustomLiveLocationTracking extends StatefulWidget {
+  const CustomLiveLocationTracking({super.key});
+
+  @override
+  State<CustomLiveLocationTracking> createState() => _CustomLiveLocationTrackingState();
+}
+
+class _CustomLiveLocationTrackingState extends State<CustomLiveLocationTracking> {
+  late CameraPosition initialCameraPosition;
+  GoogleMapController? googleMapController;
+  late Location location ;
+
+
+  @override
+  void initState() {
+    super.initState();
+    initCameraPosition();
+    location = Location();
+    updateLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        GoogleMap(
+            zoomControlsEnabled: false,
+            onMapCreated: (controller) {
+              googleMapController = controller;
+            },
+            initialCameraPosition: initialCameraPosition),
+
+      ]),
+    );
+  }
+
+  initCameraPosition() {
+    initialCameraPosition = const CameraPosition(
+        // world view 0 -> 3
+        // country view 4 -> 6
+        // city view 10 -> 13
+        // street view 14 -> 17  --- recommended
+        // building view 18 -> 20
+        zoom: 13.0,
+        target: LatLng(31.2068347767131, 29.921081125421022));
+  }
+
+  Future<void> checkAndRequestLocationService() async {
+    var isServiceEnabled = await location.serviceEnabled();
+    if(!isServiceEnabled){
+      isServiceEnabled = await location.requestService();
+      if(!isServiceEnabled){
+        // if he denied the service
+      }
+    }
+    checkAndRequestLocationPermission();
+  }
+
+  Future<bool> checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if(permissionStatus == PermissionStatus.deniedForever){
+      return false ;
+    }
+    if(permissionStatus == PermissionStatus.denied){
+      permissionStatus = await location.requestPermission();
+      if(permissionStatus != PermissionStatus.granted){
+        return false ;
+      }
+    }
+    return true ;
+  }
+
+  void getLocationData(){
+    location.onLocationChanged.listen((locationData) {
+
+      var cameraPosition = CameraPosition(target: LatLng(locationData.latitude! , locationData.longitude!));
+      googleMapController?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    });
+  }
+
+  void updateLocation()async{
+    await checkAndRequestLocationService();
+    var hasPermissions = await checkAndRequestLocationPermission();
+    if(hasPermissions){
+      getLocationData();
+    }
+  }
+
+
+}
+
+/**
+ * 1- check location service is on
+ *
+ * 2- get permission of location
+ *
+ * 3- get location
+ *
+ * 4- display
+ *
+ */
+
